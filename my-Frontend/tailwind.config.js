f827c34d71bf59d001e8b9642e1dd76509056c92
kimchi <kim24daisuki@gmail.com>
2024-12-10 09:52:22 +0700

    test

--- my-Frontend/tailwind.config.js
+++ my-Frontend/tailwind.config.js
@@ -5,7 +5,16 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+      Primary: "#f42c37",
+      Secondary: "#f42c37",
+      BrandYellow: "#fdc622",
+      BrandGreen: "#2dcc6f",
+      BrandBlue: "#1376f4",
+      BrandWhite: "#eeeeee",
+      }
+    },
   },
   plugins: [],
 }
