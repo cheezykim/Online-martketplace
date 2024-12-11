96d12f87e84df93baffb3ef91f3425c9830e7e50
kimchi <kim24daisuki@gmail.com>
2024-12-11 19:22:26 +0700

    nav done

--- my-Frontend/src/componets/Navbar/Navbar.jsx
+++ my-Frontend/src/componets/Navbar/Navbar.jsx
@@ -1,4 +1,5 @@
-import React from 'react'
+import React from 'react';
+import { IoMdSearch } from 'react-icons/io'; // Ensure this is installed: npm install react-icons
 
 const MenuLinks = [
   {
@@ -25,59 +26,55 @@ const MenuLinks = [
 
 const Navbar = () => {
   return (
-    <div className='bg-white dark:bg-gray-900 dark:text-white duration-500 relative -z-40'>  
-        <div className='py-4'> 
-            <div className="container">
-                {/*logo and links section */} 
-                <div className='flex  items-center gap-4'>  
-                    <a href="#"
-                    className='text-Primary font-semibold tracking-widest text-2xl uppercase sm:text-3xl '  
-            
+    <div className='bg-white dark:bg-gray-900 dark:text-white duration-500'>
+      <div className='py-4'>
+        <div className="container mx-auto px-4">
+          {/* Logo and Links Section */}
+          <div className='flex items-center justify-between'>
+            <a
+              href="#"
+              className='text-primary font-semibold tracking-widest text-2xl uppercase sm:text-3xl'
+            >
+              Marketplace
+            </a>
 
+            {/* Menu Items */}
+            <div className='hidden lg:block'>
+              <ul className='flex items-center gap-4'>
+                {MenuLinks.map((data) => (
+                  <li key={data.id}>
+                    <a
+                      href={data.link}
+                      className='inline-block px-4 font-semibold text-gray-500 hover:text-white dark:hover:text-white duration-500'
                     >
-                       Marketplace
+                      {data.name}
                     </a>
-
-                    {/* Menu Items  */}
-                    <div className='hidden lg:block'> 
-                      <ul className='flex items-center gap-4'> 
-                       {
-                        MenuLinks.map((data, index) => (
-                          <li key={index}>
-
-                            <a 
-                            
-                            href={data.link}
-                            className='inline-block px-4 font-semibold text-gray-500 hover:text-white dark:hover:text-white duration-500'>   
-                            
-                            {data.name} 
-                            </a>   
-                          </li>
-                        )) 
-
-                       }
-                      </ul>
-                    </div>
-                    {/* Menu Items  */}
-                    <div>
-                    </div>
-                </div>
-                {/*Navbar Right  section */} 
-
-                <div>
-                {/* Search bar section  */}
-                 <div>
-                <input type="text" placeholder='Search'
-                 className='' /> 
-                 <IoMdSearch/> 
-                 </div>
-                {/* dark mod section  */}
-                 <div></div>
-                </div>
+                  </li>
+                ))}
+              </ul>
             </div>
-        </div>
-    </div> 
-  )
-}
 
-export default Navbar
+            {/* Search Bar Section */}
+            <div className='flex items-center gap-4'>
+              <div className='flex items-center bg-gray-200 dark:bg-gray-800 px-2 py-1 rounded-md'>
+                <input
+                  type="text"
+                  placeholder='Search'
+                  className='bg-transparent text-gray-500 dark:text-white focus:outline-none'
+                />
+                <IoMdSearch className='text-gray-500 dark:text-white ml-2' />
+              </div>
+
+              {/* Dark Mode Toggle Placeholder */}
+              <div>
+                {/* Add your dark mode toggle button here */}
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
