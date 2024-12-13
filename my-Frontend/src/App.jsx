bd4a43a0f19b7e558afafa10e230b2874e1a8c5c
longchheang <tlongchheang@gmail.com>
2024-12-13 16:02:25 +0700

    done Services

--- my-Frontend/src/App.jsx
+++ my-Frontend/src/App.jsx
@@ -3,12 +3,14 @@ import React from 'react'
 import Navbar from './componets/Navbar/Navbar'
 import Category from './componets/Category/Category'
 import Category2 from './componets/Category/Category2'
+import Services from './componets/Services/Services'
 const App = () => {
   return (
     <div className='bg-white dark:bg-gray-900 dark:text-white duration-200 overflow-hidden'>
       <Navbar/> 
       <Category />
       <Category2 />
+      <Services />
     </div> 
   )
 }
