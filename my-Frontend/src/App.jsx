b16f361241678464fb4ab6c717d2a9ae0f7f0b15
Norimo79 <phorsda90@gmail.com>
2024-12-19 11:22:25 +0700

    done nav bar

--- my-Frontend/src/App.jsx
+++ my-Frontend/src/App.jsx
@@ -1,6 +1,7 @@
 // eslint-disable-next-line no-unused-vars
 import React from 'react'
 import Navbar from './componets/Navbar/Navbar'
+import Hero from'./componets/Hero/Hero';
 import Category from './componets/Category/Category'
 import Category2 from './componets/Category/Category2'
 import Services from './componets/Services/Services'
@@ -8,6 +9,7 @@ const App = () => {
   return (
     <div className='bg-white dark:bg-gray-900 dark:text-white duration-200 overflow-hidden'>
       <Navbar/> 
+      <Hero/>
       <Category />
       <Category2 />
       <Services />
--- my-Frontend/src/componets/Navbar/Navbar.jsx
+++ my-Frontend/src/componets/Navbar/Navbar.jsx
@@ -1,7 +1,7 @@
 // eslint-disable-next-line no-unused-vars
 import React from 'react';
 import { IoMdSearch } from 'react-icons/io'; // Ensure this is installed: npm install react-icons
-
+import { FaCaretDown } from "react-icons/fa";
 const MenuLinks = [
   {
     id: 1,
@@ -24,6 +24,25 @@ const MenuLinks = [
     link: "/#Blog",
   },
 ];
+const DropdownLinks =[
+  {
+    id:1,
+    name:"Trending Products",
+    link:"/#",
+},
+{
+  id:2,
+  name:"Best Selling",
+  link:"/#",
+   
+},
+{
+  id:3,
+  name:"Top Rated",
+  link:"/#",
+   
+},
+]
 
 const Navbar = () => {
   return (
@@ -52,8 +71,31 @@ const Navbar = () => {
                     </a>
                   </li>
                 ))}
+                 {/*Dropdown*/}
+                 <li className="relative cursor-pointer group">
+                  <a href='#' className="flex items-center gap-[2px] font-semibold text-gray-500 dark:hover:text-white py-2">
+                    QuickLinks
+                    <span>
+                  <FaCaretDown className="group-hover:rotate-180 duration-300"/>
+                  </span>
+                  </a>
+              {/*Dropdown Links*/}
+                <div className="absolute z-[9999] hidden group-hover:block w-[200px] rounded-md bg-white shadow-md dark:bg-gray-900 p-2 text-black dark:text-white">
+                  <ul className="space-y-2">
+                  {DropdownLinks.map((data, index) => (
+                       <li key={index}> 
+                          <a 
+                          className="text-gray-500  hover:text-black dark:hover:text-white duration-200 inline-block w-full p-2 hover:bg-Primary/20 rounded-md font-semibold"
+                          href={data.link}>{data.name}</a> 
+                          </li>
+                           ))}
+                  </ul>
+                </div>
+                </li>
               </ul>
             </div>
+            
+
 
             {/* Search Bar Section */}
             <div className='flex items-center gap-4'>
